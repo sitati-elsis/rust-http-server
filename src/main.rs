e356f5b053e2e9a67a7064e4f7e8db0f40e13ad5
sitati-elsis <sitatielsis@gmail.com>
2022-11-17 13:02:02 +0300

    Establish a tcp connection

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ mod http;
 mod server;
 
 fn main() {
-   let server = Server::new("127.0.0.1:8000".to_string());
+   let server = Server::new("127.0.0.1:8080".to_string());
    server.run();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,8 @@
+
+
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -11,6 +16,27 @@ impl Server {
     
     pub fn run(self){
         println!("Listenin on {} ", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept(){
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved to r request: {} ", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => {
+                            println!("Failed to read from connections: {}", e);
+                        }
+                   }
+
+                }
+                Err(e) => {
+                    println!("Failed to establish a connection: {} ", e);
+                }
+            }
+        }
 
     }
 }
